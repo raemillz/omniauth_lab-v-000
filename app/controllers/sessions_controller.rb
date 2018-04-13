96ae86fa4c5a30d609dad3874707b0ac33ecd395
Rachel Miller <raeanne1991@gmail.com>
2018-04-13 03:17:46 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-      user = User.find_or_create_by(uid: auth['uid']) do |user|
-        user.name = auth['info']['name']
+      user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
       end
       session[:user_id] = user.try(:id)
     end
